5dca9e2803807b245fff2c79f129c3c24f2b5e72
HubertCucumberdale <slaughterwg@gmail.com>
2024-11-19 22:35:39 -0500

    * Mapping pins for breakout board * Creating light zone header

---
+++ include/pins.h
@@ -0,0 +1,41 @@
+#ifndef Pins_Xmas_Village_h
+#define Pins_Xmas_Village_h
+
+// 5V
+#define BREAKOUT_PIN_7
+#define BREAKOUT_PIN_8
+
+// Digital Pins
+#define BREAKOUT_PIN_9 (23)
+#define BREAKOUT_PIN_10 (22)
+#define BREAKOUT_PIN_11 (25)
+#define BREAKOUT_PIN_12 (24)
+#define BREAKOUT_PIN_13 (27)
+#define BREAKOUT_PIN_14 (26)
+#define BREAKOUT_PIN_15 (29)
+#define BREAKOUT_PIN_16 (28)
+#define BREAKOUT_PIN_17 (31)
+#define BREAKOUT_PIN_18 (30)
+#define BREAKOUT_PIN_19 (33)
+#define BREAKOUT_PIN_20 (32)
+#define BREAKOUT_PIN_21 (35)
+#define BREAKOUT_PIN_22 (34)
+#define BREAKOUT_PIN_23 (37)
+#define BREAKOUT_PIN_24 (36)
+#define BREAKOUT_PIN_25 (39)
+#define BREAKOUT_PIN_26 (38)
+#define BREAKOUT_PIN_27 (41)
+#define BREAKOUT_PIN_28 (40)
+#define BREAKOUT_PIN_29 (43)
+#define BREAKOUT_PIN_30 (42)
+#define BREAKOUT_PIN_31 (45)
+#define BREAKOUT_PIN_32 (44)
+#define BREAKOUT_PIN_33 (47)
+#define BREAKOUT_PIN_34 (46)
+#define BREAKOUT_PIN_35 (49)
+#define BREAKOUT_PIN_36 (48)
+#define BREAKOUT_PIN_37 (51)
+#define BREAKOUT_PIN_38 (50)
+#define BREAKOUT_PIN_39 (53)
+#define BREAKOUT_PIN_40 (52)
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
+#include "pins.h"
 
-// put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ pinMode(LED_BUILTIN, OUTPUT);
+ pinMode(BREAKOUT_PIN_10, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on
+  digitalWrite(BREAKOUT_PIN_10, HIGH);
+  delay(500);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off
+  digitalWrite(BREAKOUT_PIN_10, LOW);
+  delay(500);                       // wait for half a second
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+/* int myFunction(int x, int y) {
   return x + y;
-}
+} */
